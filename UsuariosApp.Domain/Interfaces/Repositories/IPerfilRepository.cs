4aabed5a9007d5a8013cca38a81a4b45b27b8276
JoaoGuilhermino <jvgcosta.bt@gmail.com>
2025-08-28 21:56:22 -0300

    Criação de interfaces, infraestrutura e containers com o docker.

---
+++ UsuariosApp.Domain/Interfaces/Repositories/IPerfilRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosApp.Domain.Entities;
+
+namespace UsuariosApp.Domain.Interfaces.Repositories
+{
+    public interface IPerfilRepository
+    {
+        /// <summary>
+        /// Método para obter 1 perfil pelo nome.
+        /// </summary>
+        Perfil? GetByNome(string nome);
+    }
+}
---
+++ UsuariosApp.Domain/Interfaces/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosApp.Domain.Entities;
+
+namespace UsuariosApp.Domain.Interfaces.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        /// <summary>
+        /// Método para verificar se um email já existe no Banco de dados
+        /// </summary>
+        bool VerifyEmailExists(string email);
+
+        /// <summary>
+        /// Método para obter 1 usuário pelo email e senha.
+        /// </summary>
+        Usuario? GetByEmailAndSenha(string email, string senha);
+    }
+}
